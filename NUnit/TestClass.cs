ce231fda87bd1ea67d3b1a688b6f453e595a45d1
Dozer <mail@dozer.cc>
2012-11-27 14:43:11 +0800

    Add NUnit

---
+++ NUnit/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+using TestDll;
+
+namespace NUnit
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+            var test = new Mock<TestClass>();
+            test.Setup(t => t.NormalMethod()).Returns("Mock");
+            test.Setup(t => t.VirtualMethod()).Returns("Mock");
+            test.Setup(t => t.SealedMethod()).Returns("Mock");
+            test.Setup(t => t.AbstractMethod()).Returns("Mock");
+
+            Assert.AreEqual(test.Object.NormalMethod(), "Mock");
+            Assert.AreEqual(test.Object.VirtualMethod(), "Mock");
+            Assert.AreEqual(test.Object.SealedMethod(), "Mock");
+            Assert.AreEqual(test.Object.AbstractMethod(), "Mock");
+        }
+    }
+}
